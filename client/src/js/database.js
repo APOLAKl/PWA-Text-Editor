c6bc3903424b633679524c6012bbaa148afc26ae
APOLAKl <122183428+APOLAKl@users.noreply.github.com>
2023-05-31 22:16:58 -0300

    Third commit - fixed @babel/plugin-proposal-object-rest-spread error

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -21,6 +21,7 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   //add
+  
 }
 
 initdb();
